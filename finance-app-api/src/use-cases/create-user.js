a15a8b382bdee05451851b9bcbac8f7da21b6041
maryson gleison <maryson011@gmail.com>
2024-08-03 06:33:06 -0300

    feat: add GetUserByEmail repository

--- finance-app-api/src/use-cases/create-user.js
+++ finance-app-api/src/use-cases/create-user.js
@@ -1,10 +1,21 @@
 import { v4 as uuidv4} from 'uuid'
 import bcrypt from 'bcrypt'
 import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-use-by-email.js'
 
 export class CreateUserUseCase {
     async execute(createUserParams) {
         // TODO verificar se o e-mail já está em uso
+        const postgresGetUserByEmailRepository = new PostgresGetUserByEmailRepository()
+
+        const userWithProvidedEmail = 
+            await postgresGetUserByEmailRepository.execute(
+                createUserParams.email
+            )
+        
+        if (userWithProvidedEmail) {
+            throw new Error('The provided e-mail is already in use.')
+        }
         // gerar id usuário
         const userId = uuidv4()
         // criptografar a senha
